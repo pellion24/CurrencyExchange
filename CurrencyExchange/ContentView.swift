d8d367dc199419aa4034535a1ba941636221075d
pellion24 <ashvijhosdurg@gmail.com>
2025-04-09 11:14:34 -0400

    final project

--- CurrencyExchange/ContentView.swift
+++ CurrencyExchange/ContentView.swift
@@ -12,37 +12,51 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var viewModel = API()
-    @State private var baseCurrency = "USD"
-
+    @State private var baseCurrency = ""
+    
     @State private var countryNames = ["USD", "EUR", "GBP", "JPY", "CHF", "CAD", "AUD", "NZD", "BRL", "INR"]
     @State private var countryFlags = ["usd", "eur", "gbp", "jpy", "chf", "cad", "aud", "nzd", "brl", "inr"]
 
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Enter your base currency")
-                
-                
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-                Text("Select the country you are visiting")
-                List(countryNames, id: \.self) { country in
-                    NavigationLink(destination: ExchangeRateView(currency: country, rate: viewModel.rates[country.lowercased()] ?? 0.0, base: baseCurrency)) {
-                        HStack {
-                            Text(country)
-                            Spacer()
-                            Image(country.lowercased())
-                                .resizable()
-                                .frame(width: 40, height: 30)
+            ZStack {
+                //Image("travelBackground")
+                    //.opacity(0.3)
+                VStack {
+                    Text("1. Enter your base currency (ALL CAPS)")
+                        .font(.headline)
+                    Text("It may take some time for API request to complete, first check if your currency is 1 to 1")
+                        .font(.subheadline)
+                        .fontWidth(.compressed)
+                        
+                    TextField("", text: $baseCurrency)
+                        .onSubmit {
+                            viewModel.fetchRates(for: baseCurrency.lowercased())
+                        }
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+    
+                    
+                    Text("2. Select the country you are visiting")
+                        .font(.headline)
+                    List(countryNames, id: \.self) { country in
+                        NavigationLink(destination: ExchangeRateView(currency: country, rate: viewModel.rates[country.lowercased()] ?? 0.0, base: baseCurrency)) {
+                            HStack {
+                                Text(country)
+                                Spacer()
+                                Image(country.lowercased())
+                                    .resizable()
+                                    .frame(width: 40, height: 30)
+                            }
                         }
                     }
+                    
                 }
-            }
-            .onAppear {
-                viewModel.fetchRates(for: baseCurrency.lowercased())
-            }
-            .padding()
+                .onAppear {
+                    viewModel.fetchRates(for: baseCurrency.lowercased())
+                }
+                .padding()
+                
+            } //ZStack
         }
     }
 }
--- CurrencyExchange/ExchangeRateView.swift
+++ CurrencyExchange/ExchangeRateView.swift
@@ -12,19 +12,47 @@ struct ExchangeRateView: View {
     let currency: String
     let rate: Double
     let base: String
+    
+    @State private var amount: Double = 100.0
 
     var body: some View {
-        VStack {
-            Text("\(currency) Exchange Rate")
-                .font(.title)
-                .padding()
-            Text("1 \(base) = \(rate, specifier: "%.4f") \(currency)")
-                .font(.largeTitle)
-                .padding()
+        ZStack {
+            //Image("travelBackground")
+                //.opacity(0.3)
+            VStack {
+                HStack {
+                    Text(String(format: "%.2f", (amount)) + " \(base)")
+                    Image(base.lowercased())
+                        .resizable()
+                        .frame(width: 32, height: 22)
+                }
+                Slider(value: $amount, in: 0...1000)
+                let total = (amount * rate * 100).rounded() / 100
+                HStack {
+                    Text("You have \(String(format: "%.2f", total)) \(currency)")
+                    Image(currency.lowercased())
+                        .resizable()
+                        .frame(width: 32, height: 22)
+                    
+                }
+                    Text("\(currency) Exchange Rate")
+                        .font(.title)
+                        .padding()
+                
+                Text("1 \(base) = \(rate, specifier: "%.2f") \(currency)")
+                    .font(.largeTitle)
+                    .background(Color.yellow)
+                    .clipShape(Capsule())
+                    
+                    .padding()
+                
+                
+            }
+            
         }
     }
 }
 
 #Preview {
-    ExchangeRateView(currency: "usd", rate: 6.0, base: "credits")
+    ExchangeRateView(currency: "USD", rate: 6.0, base: "credits")
 }
